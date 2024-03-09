a7e774efc0023ed4c04d389404f1fa8198150d5d
lolapaul <150947388+lolapaul@users.noreply.github.com>
2024-03-09 15:47:44 -0600

    new file:   .DS_Store 	new file:   .streamlit/config.toml 	modified:   README.md 	new file:   app.py 	new file:   notebooks/EDA.ipynb 	new file:   requirements.txt 	new file:   vehicles_us.csv

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # project_sprint5
+# La aplicación sirve para crear un diagrama de dispersion y un histograma de anuncios de ventas de coche. Se crearon dos botones para que se generen dichas gráficos
---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Anuncios de venta de coches en EE. UU.')  # escribir un encabezado
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+disp_button = st.button('Construir diagrama de dispersión')  # crear un botón
+
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+elif disp_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un diagrama de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
